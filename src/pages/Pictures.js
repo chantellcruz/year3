f13d69036cb823221153ca3a501d2428bb639013
Chantell <chantellraecruz@gmail.com>
2023-01-31 15:13:22 -0600

    added import for carousel to work

--- src/pages/Pictures.js
+++ src/pages/Pictures.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Rainier from '../assets/rainier.png';
 import Rainier2 from '../assets/rainier2.png';
 import Batuu from '../assets/batuu.png';
