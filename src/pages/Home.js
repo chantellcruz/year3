4fd0705f286e2adae8bf4484947583dbe0dfd0ea
Chantell <chantellraecruz@gmail.com>
2023-02-01 11:46:55 -0600

    last major update. basically complete!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,7 @@ import '../styles/Home.css';
 import Container from 'react-bootstrap/Container';
 import Lego from '../assets/lego-heart.gif';
 
-
-
-export default function Home() {
-  const handleClickScroll = () => {
-    const element = document.getElementById('stats');
-    if (element) {
-      // 👇 Will scroll smoothly to the top of the next section
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+function Home() {
 
   const scrollToStats = () => {
     const element = document.getElementById('stats');
@@ -22,15 +13,16 @@ export default function Home() {
     }
   };
 
-
   return (
     <div id="home">
       <Container>
         <img src={Lego} width="800px" />
         <h1>Yay we made it 3 years</h1>
         <p>wowowowow. look at us.</p>
-        <button className="btn-scroll" onClick={scrollToStats} style={{outline: 'none'}}>Let's see some stats</button>
+        <button className="btn-scroll" onClick={scrollToStats} style={{outline: 'none'}}>let's see some stats</button>
       </Container>
     </div>
   );
 }
+
+export default Home;
--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,43 +1,117 @@
 import React from 'react';
 import '../styles/Stats.css';
+import CountUp, { useCountUp } from 'react-countup';
+import VisibilitySensor from "react-visibility-sensor";
 
 function Stats() {
-  
+
+  useCountUp({
+    ref: 'counter',
+    end: 1234567,
+    enableScrollSpy: true,
+    scrollSpyDelay: 1000,
+  });
+
   return (
     <div id="stats">
       <h1>Our relationship by the numbers</h1>
       <h3>This feels so very type A of me - I'm sorry.</h3>
+
       <div className="flex-container">
-      <div id="column">
-  <span class="num" data-val="49"><h1>0</h1></span>
-    <p>months as friends</p>
+        <div id="column">
+          <CountUp end={49} duration={2.45} enableScrollSpy
+            style={{
+              fontSize: '80px',
+              letterSpacing: '-2px',
+              color: '#e6a28b',
+              fontFamily: 'poppins, sans-serif',
+              fontWeight: 600
+            }} />
+          <p>months of being friends</p>
+        </div>
+        <div id="column">
+          <CountUp end={36} duration={1.8} enableScrollSpy
+            style={{
+              fontSize: '80px',
+              letterSpacing: '-2px',
+              color: '#e6a28b',
+              fontFamily: 'poppins, sans-serif',
+              fontWeight: 600
+            }} />
+          <p>months of dating</p>
+        </div>
+        <div id="column">
+        <CountUp end={8} duration={1} enableScrollSpy
+            style={{
+              fontSize: '80px',
+              letterSpacing: '-2px',
+              color: '#e6a28b',
+              fontFamily: 'poppins, sans-serif',
+              fontWeight: 600
+            }} />
+          <p>national parks visited</p>
+        </div>
+        <div id="column">
+        <CountUp end={1038} duration={4} enableScrollSpy
+            style={{
+              fontSize: '80px',
+              letterSpacing: '-2px',
+              color: '#e6a28b',
+              fontFamily: 'poppins, sans-serif',
+              fontWeight: 600
+            }} />
+          <p>miles apart from each other</p>
+        </div>
+      </div>
+
+      <div className="flex-container">
+        <div id="column">
+          <CountUp end={6} duration={1} enableScrollSpy
+            style={{
+              fontSize: '80px',
+              letterSpacing: '-2px',
+              color: '#e6a28b',
+              fontFamily: 'poppins, sans-serif',
+              fontWeight: 600
+            }} />
+          <p>halo campaigns played together</p>
+        </div>
+        <div id="column">
+          <CountUp end={13} duration={2} enableScrollSpy
+            style={{
+              fontSize: '80px',
+              letterSpacing: '-2px',
+              color: '#e6a28b',
+              fontFamily: 'poppins, sans-serif',
+              fontWeight: 600
+            }} />
+          <p>in-person baseball games</p>
+        </div>
+        <div id="column">
+        <CountUp end={15} duration={2} enableScrollSpy
+            style={{
+              fontSize: '80px',
+              letterSpacing: '-2px',
+              color: '#e6a28b',
+              fontFamily: 'poppins, sans-serif',
+              fontWeight: 600
+            }} />
+          <p>states visited</p>
+        </div>
+        <div id="column">
+        <CountUp end={8} duration={1} enableScrollSpy
+            style={{
+              fontSize: '80px',
+              letterSpacing: '-2px',
+              color: '#e6a28b',
+              fontFamily: 'poppins, sans-serif',
+              fontWeight: 600
+            }} />
+          <p>gaming events</p>
+        </div>
+      </div>
     </div>
-  <div id="column">
-  <span class="num" data-val="36"><h1>0</h1></span>
-    <p>months of dating</p>
-    </div>
-  <div id="column">
-  <span class="num" data-val="8"><h1>0</h1></span>
-    <p>national parks visited</p>
-    </div>
-  
-</div>
-<div className="flex-container">
-<div id="column">
-  <span class="num" data-val="6"><h1>0</h1></span>
-    <p>halo campaigns played together</p>
-    </div>
-  <div id="column">
-  <span class="num" data-val="15"><h1>0</h1></span>
-    <p>states visited</p>
-    </div>
-  <div id="column">
-  <span class="num" data-val="8"><h1>0</h1></span>
-    <p>gaming events</p>
-    </div>
-</div>
-    </div>
-    
+
   )
 }
 
